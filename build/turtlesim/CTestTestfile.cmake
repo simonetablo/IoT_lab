50a591f41678e1ab29f3d48719a169c9cf85d92e
simone <simone.tablo@gmail.com>
2024-04-04 17:45:15 +0200

    First commit

---
+++ build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/simone/Documents/IoT/src/ros_tutorials/turtlesim
+# Build directory: /home/simone/Documents/IoT/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
